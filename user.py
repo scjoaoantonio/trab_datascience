571905024fb0e98433aef737b45d23fe0ca0c8b0
João Antônio <joaoasccc@aluno.ufsj.edu.br>
2025-01-23 09:13:25 -0300

    frontend

--- user.py
+++ user.py
@@ -18,7 +18,7 @@ def usersPage():
 
     actor = st.text_input("Digite o @ do usuário:", value="cruzeiro.com.br", key="actor_input")
     limit = st.number_input("Quantidade de posts por iteração:", min_value=1, max_value=100, value=10, key="limit_input")
-    iterations = st.number_input("Número de iterações:", min_value=1, max_value=10, value=3, key="iterations_input")
+    iterations = st.number_input("Número de iterações:", min_value=1, max_value=100, value=3, key="iterations_input")
 
     if st.button("Analisar", key="analyze_button"):
         if actor:
@@ -31,6 +31,22 @@ def usersPage():
                 # Criar DataFrame
                 df = pd.DataFrame(posts)
 
+                # Baixar CSV
+                st.write("### Baixar Dados como CSV")
+                csv = df.to_csv(index=False)
+                st.download_button(label="Baixar CSV", data=csv, file_name="dados_posts.csv", mime="text/csv")
+
+                # WordCloud
+                st.write("### WordCloud das Palavras Mais Frequentes")
+                generate_wordcloud(df['tokens'])
+
+                # Gráfico de evolução temporal
+                st.write("### Evolução Temporal de Engajamento")
+                df['data_hora'] = pd.to_datetime(df['data_hora'])
+                temporal_data = df.groupby(df['data_hora'].dt.date)['total'].sum()
+                st.line_chart(temporal_data)
+
+
                 # Gráfico de distribuição de valores
                 st.write("### Distribuição dos Valores")
                 distribution_values(df)
@@ -39,26 +55,17 @@ def usersPage():
                 st.write("### Correlação Entre os Atributos")
                 analyze_correlation(df)
 
-                # WordCloud
-                st.write("### WordCloud das Palavras Mais Frequentes")
-                generate_wordcloud(df['tokens'])
-
                 # Tokens com mais engajamento
                 st.write("### Tokens com Mais Engajamento")
                 top_tokens = get_top_tokens(df)
                 st.dataframe(top_tokens)
-
-                # Baixar CSV
-                st.write("### Baixar Dados como CSV")
-                csv = df.to_csv(index=False)
-                st.download_button(label="Baixar CSV", data=csv, file_name="dados_posts.csv", mime="text/csv")
 
                 # Posts com mais engajamento
                 st.write("### Posts com Mais Engajamento")
                 top_posts = df.sort_values(by='total', ascending=False).head(5)
 
                 for _, row in top_posts.iterrows():
-                    st.write(f"**Nome:** {row['author_displayName']} - **Usuário:** {row['author_handle']}")
+                    st.write(f"**{row['author_displayName']}** (@{row['author_handle']}) - **Engajamento:** {row['total']}")
                     st.write(f"**Texto Original:** {row['texto_original']}")
 
                     # Verificar se há imagens associadas ao post
@@ -69,13 +76,7 @@ def usersPage():
                             if image_url:
                                 st.image(image_url, caption="Imagem do Post", use_column_width=True)
 
-                    st.write(f"**Engajamento Total:** {row['total']}\n---")
-
-                # Gráfico de evolução temporal
-                st.write("### Evolução Temporal de Engajamento")
-                df['data_hora'] = pd.to_datetime(df['data_hora'])
-                temporal_data = df.groupby(df['data_hora'].dt.date)['total'].sum()
-                st.line_chart(temporal_data)
+                    # st.write(f"**Engajamento Total:** {row['total']}\n---")
 
             else:
                 st.error("Nenhum dado encontrado para o usuário informado.")
